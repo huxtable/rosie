5b6cac4760acf93649e4ad218ea994fef7911b8c
Ashur Cabrera <hello@cabreramade.co>
2016-06-25 08:24:57 -0700

    updating bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,7 @@
 sudo apt-get update -y
 
 # Data directories
+sudo mkdir /var/opt/rosie
 sudo chown -R vagrant /var/opt/rosie
 sudo chgrp -R vagrant /var/opt/rosie
 
@@ -12,3 +13,5 @@ sudo ln -s /vagrant/bin/rosie /usr/local/bin/rosie
 
 # Install dependencies
 sudo apt-get install php5-cli -y
+sudo apt-get install php5-curl -y
+sudo apt-get install php5-imagick -y
